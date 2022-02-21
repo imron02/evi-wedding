f02100ec4ff107ac869858890b999eee09e58e2f
Imron Rosdiana <imron02@gmail.com>
2022-02-21 21:45:18 +0700

    update cover image

--- components/home/header_open.tsx
+++ components/home/header_open.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Button } from "react-bootstrap";
 
 const Header = styled.section`
   &:before {
