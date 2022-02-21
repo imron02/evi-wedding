9ea1f07ce37a780882b47b0ce1f4348eb854b5a9
Imron Rosdiana <imron02@gmail.com>
2022-02-21 21:36:35 +0700

    revert og image

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -42,9 +42,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="og:title" content="Evi &amp; Bosnia Wedding" />
         <meta property="og:description"
               content="Evi &amp; Bosnnia akan menikah pada hari Sabtu, 5 Maret 2022" />
-        {/*<meta property="og:image"*/}
-        {/*      content="https://evi-bosnia.vercel.app/images/cover.jpeg" />*/}
-        <meta property="og:image" content="https://demo.datengdong.com/uploads/thumbs/gallery/1949d9111354e8962623050b26440179f5caddd0MS.jpg" />
+        <meta property="og:image"
+              content="https://evi-bosnia.vercel.app/images/cover.jpeg" />
         <meta property="og:image:alt" content="Evi &amp; Bosnia Wedding Image" />
         <link href="https://fonts.cdnfonts.com/css/quicksand" rel="stylesheet" />
         <link href="https://fonts.cdnfonts.com/css/chopin-script" rel="stylesheet" />
